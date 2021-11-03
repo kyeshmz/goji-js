c66ee42970398dbc7cf797afc66eb59804966b06
Malash <i@malash.me>
2021-11-03 10:30:55 +0800

    use path.posix

--- packages/webpack-plugin/src/utils/path.ts
+++ packages/webpack-plugin/src/utils/path.ts
@@ -9,10 +9,10 @@ export const safeUrlToRequest = (url: string) => (isUrlRequest(url) ? urlToReque
  * @param pathname like `a/b/c`
  * @returns relativePathToBridge like `../../..`
  */
-export const getRelativePathToBridge = (pathname: string, basrdir: string) => {
+export const getRelativePathToBridge = (pathname: string, basedir: string) => {
   if (pathname.startsWith('/')) {
     throw new Error('`pathname` should not be absolute path');
   }
-  const relativePath = path.relative(path.dirname(pathname), basrdir);
-  return safeUrlToRequest(path.join(relativePath, BRIDGE_OUTPUT_DIR));
+  const relativePath = path.posix.relative(path.posix.dirname(pathname), basedir);
+  return safeUrlToRequest(path.posix.join(relativePath, BRIDGE_OUTPUT_DIR));
 };
