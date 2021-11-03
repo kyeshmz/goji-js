33ea8c093756ec855a1b56bea7ccce2f6b6624e9
Malash <i@malash.me>
2021-11-03 11:00:56 +0800

    implement posix version of replace-ext

--- packages/webpack-plugin/src/utils/path.ts
+++ packages/webpack-plugin/src/utils/path.ts
@@ -16,3 +16,29 @@ export const getRelativePathToBridge = (pathname: string, basedir: string) => {
   const relativePath = path.posix.relative(path.posix.dirname(pathname), basedir);
   return safeUrlToRequest(path.posix.join(relativePath, BRIDGE_OUTPUT_DIR));
 };
+
+// `replace-ext` using posix style
+// forked from https://github.com/gulpjs/replace-ext/blob/fa419a29bd7c6347dbd695bbac00ec062ed4f70d/index.js
+function startsWithSingleDot(fpath) {
+  const first2chars = fpath.slice(0, 2);
+  return first2chars === `.${path.sep}` || first2chars === './';
+}
+
+export const replaceExtPosix = (npath: string, ext: string) => {
+  if (typeof npath !== 'string') {
+    return npath;
+  }
+
+  if (npath.length === 0) {
+    return npath;
+  }
+
+  const nFileName = path.posix.basename(npath, path.posix.extname(npath)) + ext;
+  const nFilepath = path.posix.join(path.dirname(npath), nFileName);
+
+  if (startsWithSingleDot(npath)) {
+    return `.${path.sep}${nFilepath}`;
+  }
+
+  return nFilepath;
+};
